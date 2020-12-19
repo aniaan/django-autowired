9ebd4c148243da4761065f923aa2face66a0bbc7
BeanNan <fjn.aniaan@gmail.com>
2020-12-20 00:01:24 +0800

    test(form): add form test (#6)

--- django_autowired/typing.py
+++ django_autowired/typing.py
@@ -1,8 +1,10 @@
-from typing import Any, Callable
+from typing import Any
+from typing import Callable
+from typing import Iterable
+from typing import Type
 from typing import Union
-from typing import Mapping
-from typing import List
 
+from django.core.files.uploadedfile import UploadedFile as DjangoUploadFile
 from django.utils.datastructures import MultiValueDict
 
 ViewFunc = Callable
@@ -10,3 +12,15 @@ ViewFunc = Callable
 # Waiting for mypy to support recursion
 JSON = Any
 BodyType = Union[JSON, MultiValueDict]
+
+
+class UploadFile(DjangoUploadFile):
+    @classmethod
+    def __get_validators__(cls: Type["UploadFile"]) -> Iterable[Callable]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls: Type["UploadFile"], v: Any) -> Any:
+        if not isinstance(v, DjangoUploadFile):
+            raise ValueError(f"Expected UploadFile, received: {type(v)}")
+        return v
